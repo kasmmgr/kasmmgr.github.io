f2a572544150b2ed41c16609f94ac9004a4cff9a
casjay <casjay@yahoo.com>
2024-10-13 01:13:01 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'CasjaysDev',
+    description: 'CasjaysDev store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://kasmmgr.github.io/',
+    contactUrl: 'https://github.com/kasmmgr/kasmmgr.github.io/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
